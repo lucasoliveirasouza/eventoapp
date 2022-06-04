7d6e55e7f29f03992e70ccbc95ba2c01ed238f69
lucasoliveirasouza <>
2022-06-04 13:55:53 -0300

    Criar estrututra mvc #1 #2

--- eventoapp/src/main/java/br/com/eventoapp/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/br/com/eventoapp/eventoapp/EventoappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventoappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventoappApplication.class, args);
 	}
--- eventoapp/src/main/java/br/com/eventoapp/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/br/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -1,8 +1,13 @@
 package br.com.eventoapp.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EventoController {
+    @RequestMapping("/cadastrarEvento")
+    public String form(){
+        return "evento/formEvento";
+    }
 
 }
